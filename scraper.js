d56c8b5b0b6f86ace494522e742740c11b81b2a1
Mraleksa <a.gribanovskiy@gmail.com>
2018-05-05 09:25:55 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,2 +1,7 @@
 console.log('hi');
 
+const db = require('monk')("mongodb://MORPH_MONGO@ds111078.mlab.com:11078/cep")
+const users = db.get('database')
+
+users.insert({ name: 'hi',quote: ' mongo!'})
+
