f8c60f75a99b0b010da7cfe53ced697b3befa5b0
Mraleksa <a.gribanovskiy@gmail.com>
2018-05-05 09:45:50 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -1,6 +1,7 @@
 console.log('hi');
+my_secret_value = process.env.MORPH_MYSECRET;
 
-const db = require('monk')(MORPH_MONGO)
+const db = require('monk')(my_secret_value)
 const users = db.get('database')
 
 users.insert({ name: 'hi',quote: ' mongo4'})
